2de4200dde1a1ec7459f7c6ccd49e8503b04a478
Brandon Montgomery <bmonty@gmail.com>
2011-02-15 12:40:20 -0500

    improving parsing logic

--- cssparser.rb
+++ cssparser.rb
@@ -8,6 +8,13 @@ filePath = ARGV[0]
 parser = CssParser::Parser.new
 parser.load_file!(filePath)
 
-parser.each_selector() do |selector, declarations, specificity|
-  puts selector if /^\./.match(selector)
+parser.each_selector do |selector, declarations, specificity|
+  matches = selector.scan(/\.([a-z0-9-_]+)/i)
+  if matches.length > 0 then
+    matches.each do |match|
+      puts match
+    end
+  end
 end
+
+# now we need to look in a certain directory, at certiain file types, and see if each class is used. if it's not, let the user know they can most likely get rid of it
