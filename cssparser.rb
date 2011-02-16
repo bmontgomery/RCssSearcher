711d7af1dcb12ef72140bb09c7d2be5d21b26b56
Brandon Montgomery <bmonty@gmail.com>
2011-02-16 08:39:21 -0500

    uncluttering output a bit

--- cssparser.rb
+++ cssparser.rb
@@ -5,7 +5,7 @@ include CssParser
 
 filePath = ARGV[0]
 searchDirectory = ARGV[1]
-puts "filePath: #{filePath}, searchDirectory: #{searchDirectory}"
+puts "CSS file: #{filePath}, search directory: #{searchDirectory}"
 
 if searchDirectory.nil? or searchDirectory.length == 0 then
   searchDirectory = "*"
@@ -18,12 +18,10 @@ parser.each_selector do |selector, declarations, specificity|
   matches = selector.scan(/\.([a-z0-9\-_]+)/i)
   if matches.length > 0 then
     matches.each do |match|
+      puts "searching for \"#{match[0]}\""
       match[0].gsub!("-", "\\-")
       grepCommand = "grep -riI #{match[0]} \"#{searchDirectory}\""
-      puts grepCommand
       system grepCommand
     end
   end
 end
-
-# now we need to look in a certain directory, at certiain file types, and see if each class is used. if it's not, let the user know they can most likely get rid of it
