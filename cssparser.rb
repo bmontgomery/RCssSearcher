2d1f563af84cc1b97f9dc184fba329dc7d646a3e
Brandon Montgomery <bmonty@gmail.com>
2011-02-16 10:14:23 -0500

    display each css class and the numberof files it is used in, ordered by the nubmer of files each is used in ascending

--- cssparser.rb
+++ cssparser.rb
@@ -14,16 +14,25 @@ end
 parser = CssParser::Parser.new
 parser.load_file!(filePath)
 
+searchTerms = {}
+
 parser.each_selector do |selector, declarations, specificity|
   matches = selector.scan(/\.([a-z0-9\-_]+)/i)
   if matches.length > 0 then
     matches.each do |match|
-      print "searching for \"#{match[0]}\"..."
-      match[0].gsub!("-", "\\-")
-      output = `grep -riIn --exclude-dir=\"\\.svn\" --exclude=\"*.css\" #{match[0]} \"#{searchDirectory}\"`
-      outlines = output.split("\n")
-      print output == "" ? "not found" : "found #{outlines.length} times"
-      print "\n"
+      term = String.new(match[0])
+      if searchTerms[term].nil? then
+        puts "searching for \"#{term}\"..."
+        match[0].gsub!("-", "\\-")
+        output = `grep -riIn --exclude-dir=\"\\.svn\" --exclude=\"*.css\" #{match[0]} \"#{searchDirectory}\"`
+        outlines = output.split("\n")
+        searchTerms[term] = outlines.length
+      end
     end
   end
+end
+
+sortedTerms = searchTerms.sort{|a,b| a[1] <=> b[1]}
+sortedTerms.each do |term| 
+  puts "found \"#{term[0]}\" in #{term[1]} files"
 end
