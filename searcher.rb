986ee588a86df97d845fd6d70af6ff02e9e117e0
Andrew Stock <totally@funoninter.net>
2011-07-06 23:59:49 -0400

    added file path normalization and checking of program arguments

--- searcher.rb
+++ searcher.rb
@@ -1,11 +1,30 @@
-imageDirectory = String.new(ARGV[0])
-searchDirectory = ARGV[1]
-imageDirectory.gsub!("\\", "/")
-puts "image directory: #{imageDirectory}, search directory: #{searchDirectory}"
+# first check for incoming parameters
+if ARGV.length < 2
+	puts "Usage: searcher <image directory> <search directory>"
+	exit
+end
 
+imageDirectory = String.new(ARGV[0])
+searchDirectory = String.new(ARGV[1])
+
+# normalize file paths, substitute \ with /
+imageDirectory.gsub!("\\", "/")
+searchDirectory.gsub("\\", "/")
+
+# make paths end with trailing slashes
+if not imageDirectory.end_with?("/") then
+	imageDirectory.concat("/")
+end
+
+# we only do the ending-slash normalization for the search directory if it's non-empty
+# if the search directory is empty, we change it to "*"
 if searchDirectory.nil? or searchDirectory.length == 0 then
   searchDirectory = "*"
+elsif not searchDirectory.end_with?("/") then
+	searchDirectory.concat("/")
 end
+
+puts "image directory: #{imageDirectory}, search directory: #{searchDirectory}"
 
 # collect list of images in folder
 files = Dir.glob("#{imageDirectory}*.{jpg,png,gif}")
